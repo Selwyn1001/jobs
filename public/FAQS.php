2636d75457749286979106259347a6fd1c19669d
Selwyn1001 <79578065+Selwyn1001@users.noreply.github.com>
2023-01-17 15:53:42 +0000

    FAQS page creation

---
+++ public/FAQS.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="/styles.css"/>
+		<title>Jo's Jobs - About</title>
+	</head>
+	<body>
+	<header>
+		<section>
+			<aside>
+				<h3>Office Hours:</h3>
+				<p>Mon-Fri: 09:00-17:30</p>
+				<p>Sat: 09:00-17:00</p>
+				<p>Sun: Closed</p>
+			</aside>
+			<h1>Jo's Jobs</h1>
+
+		</section>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li>Jobs
+				<ul>
+					<li><a href="it.php">IT</a></li>
+					<li><a href="hr.php">Human Resources</a></li>
+					<li><a href="sales.php">Sales</a></li>
+
+				</ul>
+			</li>
+			<li><a href="/about.html">About Us</a></li>
+		</ul>
+
+	</nav>
+<img src="images/randombanner.php"/>
+	<main class="home">
+
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+</div>
+
+<h2>Select the type of job you are looking for:</h2>
+		<ul>
+					<li><a href="it.php">IT</a></li>
+					<li><a href="hr.php">Human Resources</a></li>
+					<li><a href="sales.php">Sales</a></li>
+
+				</ul>
+			</li>
+
+	</main>
+
+
+
+</html>
+<?php
+
+require ('foot.php');
+
+
+?>
